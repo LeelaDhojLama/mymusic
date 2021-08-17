13888ff7fb77242a259348ad45d3bcd93d136507
LeelaDhojLama <buddhalama357@gmail.com>
2021-08-17 09:04:02 +0545

    intial commit

---
+++ android/app/src/main/kotlin/com/aptcluster/my_music/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.aptcluster.my_music
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/ui/music_player_screen.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class MusicPlayerScreen extends StatefulWidget{
+  @override
+  State<StatefulWidget> createState() => _MusicPlayerScreen();
+
+}
+
+class _MusicPlayerScreen extends State<MusicPlayerScreen>{
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+    );
+  }
+
+}
